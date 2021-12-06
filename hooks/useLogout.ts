dd818dc7ba0cf8a4c3fc36c9946c3a836b4a76e6
noritakasawada <snp.inc.info@gmail.com>
2021-12-06 23:07:21 +0900

    first commit

--- hooks/useLogout.ts
+++ hooks/useLogout.ts
@@ -1,15 +1,23 @@
 import Cookie from "universal-cookie";
 import firebase from '../firebaseConfig'
 import { unSubMeta } from "./useUserChanged";
-
+import { useQueryClient, QueryClient } from 'react-query';
+import { useDispatch } from "react-redux";
+import { resetEditedNews, resetEditedTask } from '../slices/uiSlice'
 
 const cookie = new Cookie()
 export const useLogout = () => {
+    const dispatch = useDispatch()
+    const queryClient = useQueryClient()
     const logout = async () => {
         if (unSubMeta) {
             unSubMeta()
         }
+        dispatch(resetEditedTask())
+        dispatch(resetEditedTask())
         await firebase.auth().signOut()
+        queryClient.removeQueries("tasks")
+        queryClient.removeQueries("news")
         cookie.remove('token')
     }
     return { logout }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,33 @@
 import '../styles/globals.css'
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import type { AppProps } from 'next/app'
 import { useUserChanged } from '../hooks/useUserChanged';
+import { Provider } from 'react-redux'
+import { store } from '../app/store';
+
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { } = useUserChanged()
-  return <Component {...pageProps} />
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            retry: false,
+            refetchOnWindowFocus: false
+          }
+        }
+      })
+  )
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>
+        <Component {...pageProps} />
+      </Provider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  )
 }
 export default MyApp
